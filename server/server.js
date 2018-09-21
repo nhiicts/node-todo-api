79d4cf399b5c12058f941ed5b3e2ecb80f679abd
tranhongnhi <tranhongnhi1993@gmail.com>
2018-09-21 14:49:20 +0700

    =First test

--- server/server.js
+++ server/server.js
@@ -15,10 +15,14 @@ app.post('/todos', (req, res) => {
   todo.save().then((doc) => {
     res.send(doc)
   }).catch(e => {
-    res.send(e);
+    res.status(400).send(e);
   });
 });
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {
+  app
+}
