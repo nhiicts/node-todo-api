cc403a0b45bc7992be5b0d19088b744c6bc7b295
tranhongnhi <tranhongnhi1993@gmail.com>
2018-09-23 17:16:40 +0700

    Generate token

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ const dbPath = process.env.MONGODB_URI;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(dbPath, { useNewUrlParser: true }).catch(e => {
-  console.log('connect to database error', e, dbPath);
+  console.log('Error while connect to database ', e)
 }).then(() => {
   console.log('success connect to database', dbPath);
 });
--- server/server.js
+++ server/server.js
@@ -85,6 +85,16 @@ app.patch('/todos/:id', (req, res) => {
   }).catch(e => res.status(404).send(e));
 });
 
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch(e => res.status(400).send(e));
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
