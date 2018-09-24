72596d63cd662213bb087dce4aa5fbeb072b7a4b
tranhongnhi <tranhongnhi1993@gmail.com>
2018-09-24 15:14:01 +0700

    Add login api

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,24 @@ UserSchema.statics.findByToken = function (token) {
   });
 }
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  const User = this;
+  return User.findOne({email: email}).then(user => {
+    if (!user) {
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (err || !result) {
+          reject();
+        } else {
+          resolve(user);
+        }
+      });
+    });
+  });
+}
+
 UserSchema.pre('save', function (next) {
   const user = this;
   const isPasswordModified = user.isModified('password');
--- server/server.js
+++ server/server.js
@@ -96,6 +96,15 @@ app.post('/users', (req, res) => {
   }).catch(e => res.status(400).send(e));
 });
 
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then(user => {
+    res.send(user);
+  }).catch(e => {
+    res.status(400).send();
+  });
+});
+
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
