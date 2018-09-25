83e98562d4865eb2e857b21f984e71c71c690c0a
tranhongnhi <tranhongnhi1993@gmail.com>
2018-09-25 14:43:35 +0700

    add POST users/login

--- server/server.js
+++ server/server.js
@@ -99,7 +99,9 @@ app.post('/users', (req, res) => {
 app.post('/users/login', (req, res) => {
   const body = _.pick(req.body, ['email', 'password']);
   User.findByCredentials(body.email, body.password).then(user => {
-    res.send(user);
+    return user.generateAuthToken().then(token => {
+      res.header('x-auth', token).send(user);
+    });
   }).catch(e => {
     res.status(400).send();
   });
