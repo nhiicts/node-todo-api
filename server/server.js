5bc80c8018650fd49410202cb6ecfc9be833d935
tranhongnhi <tranhongnhi1993@gmail.com>
2018-09-25 15:17:31 +0700

    add delete token

--- server/server.js
+++ server/server.js
@@ -107,6 +107,12 @@ app.post('/users/login', (req, res) => {
   });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }).catch(e => res.status(400).send(e));
+});
+
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
